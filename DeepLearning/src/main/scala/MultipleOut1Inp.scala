05e9504fc0dc113e0a16f3536ca1bcc274ac3d28
Roxana Tesileanu <roxana.te@web.de>
2017-06-28 12:11:29 +0300

    testing the nnet with multiple outputs and a single output

--- DeepLearning/src/main/scala/MultipleOut1Inp.scala
+++ DeepLearning/src/main/scala/MultipleOut1Inp.scala
@@ -1,2 +1,15 @@
 object MultipleOut1Inp {
+
+def neural_network (myWeights: Array[Double], myInput : Double) : Array[Double] = {
+ 
+val myPredictions = myWeights map(i => (i*myInput)) 
+myPredictions
 }
+
+//let's test the code with Andrew's example
+
+val testMultipleOut1Inp = neural_network(Array(0.3, 0.2, 0.9), 0.65)
+
+
+}
+
