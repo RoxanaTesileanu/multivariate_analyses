038f697663c6c32e6f3f8818c68eea8b94924193
Roxana Tesileanu <roxana.te@web.de>
2017-06-28 14:28:46 +0300

    testing the nnet with multiple inputs and multiple outputs

--- DeepLearning/src/main/scala/MultipleInpOut.scala
+++ DeepLearning/src/main/scala/MultipleInpOut.scala
@@ -1,2 +1,19 @@
+import breeze.linalg._
+
+
 object MultipleInpOut {
+
+val myWeights = DenseMatrix((0.1, 0.1, -0.3), (0.1, 0.2, 0.0), (0.0, 1.3, 0.1))
+val myInputs =  DenseMatrix(Array(8.5, 0.65, 1.2)).t
+
+def neural_network (myWeights: DenseMatrix[Double], myInputs: DenseMatrix[Double]): DenseMatrix[Double] = {
+ assert(myWeights.cols == myInputs.rows, "Matrices cannot be multiplied!") 
+ val matMultiplication= myWeights * myInputs
+ matMultiplication
+}
+
+//let's test it with Andrew's example:
+
+val testMultipleInpOut = neural_network(myWeights, myInputs)
+
 }
