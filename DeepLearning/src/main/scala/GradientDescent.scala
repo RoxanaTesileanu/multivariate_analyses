3286ed3f26160c04cbe34f94be363b66a37650d5
Roxana Tesileanu <roxana.te@web.de>
2017-07-17 14:35:15 +0300

    improving my updatingWeights function to implement gradient descent

--- DeepLearning/src/main/scala/GradientDescent.scala
+++ DeepLearning/src/main/scala/GradientDescent.scala
@@ -1,24 +1,18 @@
 
 object GradientDescent{
-var weight = 0.5
-val goalPred = 0.8
-val input = 2
-val alpha = 0.1
-
-
 @annotation.tailrec
 def updatingWeights (weight: Double, goalPred:Double,
-                     input: Double, alpha :Double) : Double = {
+                     input: Double, alpha :Double) : Unit = {
 
   var prediction = input * weight
   var error = prediction - goalPred
   var derivative = input* (prediction - goalPred)
 
-
-    if (derivative < 0.01) weight
-    else updatingWeights(weight, goalPred, input, alpha)
+    if (error < 0.001) println(prediction , error, weight)
+    else updatingWeights(weight - alpha * derivative, goalPred, input, alpha)
 
 }
 
 updatingWeights(0.5, 0.8, 2, 0.1)
 }
+
