67690cc42a3418af291ebbc861eb5dbee6d46ae3
Roxana Tesileanu <roxana.te@web.de>
2017-07-18 13:26:36 +0300

    improved code for the GD for multiple inputs

--- DeepLearning/src/main/scala/GradientDescentMultiInput.scala
+++ DeepLearning/src/main/scala/GradientDescentMultiInput.scala
@@ -1,45 +1,26 @@
 object GradientDescentMultiInput {
 
-val myW = Array(0.1, 0.2, -0.1)
-val myI = Array(8.5, 0.65, 1.2)
-val myPred = neural_network(myW, myI)
-val goalPredict = Array(1,1,0,1)
-@annotation.tailrec
-def updatingWeights (myWeights: Array[Double], goalPred:Double,
-                     myInputs: Array[Double], alpha :Double) : Unit = {
+import breeze.linalg._
+import scala.math._
 
-  def predict (myInputs: Array[Double], myWeights: Array[Double]) : Double= {
-val myPairs = myWeights zip myInputs
-val myPairProd = for (p <- myPairs) yield {p._1 * p._2}
-val dotProduct = myPairProd.sum
+val myW = new DenseVector(Array(0.1, 0.2, -0.1))
+val myI = new DenseVector(Array(8.5, 0.65, 1.2))
+val goalPredict = new DenseVector(Array(1,1,0,1))
+@annotation.tailrec
+def updatingWeights (myWeights: DenseVector[Double], goalPred:Double,
+                     myInputs: DenseVector[Double], alpha :Double) : Unit = {
+
+  def predict (myInputs: DenseVector[Double], myWeights: DenseVector[Double]) : Double= {
+val dotProduct = myInputs dot myWeights
 dotProduct
 }
   var error = predict(myInputs, myWeights) - goalPredict(0)
-  var derivative = myInput* (predict(myInputs, myWeights) - goalPredict(0))
+  var derivative = myInputs* (predict(myInputs, myWeights) - goalPredict(0))
 
-    if (error < 0.001) println(predict(myInputs, myWeights) , error, myWeights)
+    if (abs(error) < 0.001) println(predict(myInputs, myWeights) , error, myWeights)
     else updatingWeights(myWeights - alpha * derivative, goalPred, myInputs, alpha)
-
+}
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
