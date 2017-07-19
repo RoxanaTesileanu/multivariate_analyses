3858962d5ecef1549dba1545dd51edc3b8240a79
Roxana Tesileanu <roxana.te@web.de>
2017-07-19 11:47:46 +0300

    working at the code for  kNN

--- DeepLearning/src/main/scala/kNN.scala
+++ DeepLearning/src/main/scala/kNN.scala
@@ -4,13 +4,15 @@ import breeze.linalg._
 import scala.math._
 import scala.util.Sorting
 
-def createData (group: DenseMatrix[Double], labels: DenseVector[Char]) = {
- val group = DenseMatrix((1.0, 1.1), (1.0, 1.0), (0.0, 0.0), (0.0, 0.1))
- val lables = DenseVector(Array('A', 'A', 'B', 'B')
- group, lables
-}
+val group = DenseMatrix((1.0, 1.1), (1.0, 1.0), (0.0, 0.0), (0.0, 0.1))
+val lables = DenseVector(Array('A', 'A', 'B', 'B'))
+case class createData (group: DenseMatrix[Double], labels: DenseVector[Char]) 
 
-def classifykNN (P:DenseVector[Double], group: DenseMatrix[Double], labels: DenseVector[Char], k: Int) = {
+val dataSet = createData(group, labels)
+ 
+
+
+class classifykNN (P:DenseVector[Double], dataSet: createData, k: Int)  {
 
 def distance (P1: DenseVector[Double], P2: DenseVector[Double]) : Double = {
   
@@ -18,9 +20,11 @@ val d = sqrt(pow(P1(0)- P2(0),2) + pow(P1(1) - P2(1),2))
 d
 }
 
-val distances = group map(x => distance(X, x))
+val distances = dataSet.group map(x => distance(P, x))
 
-distances.zip(labels)
+val labeledDist = distances.zip(labels)
+
+def sortingDist (labeledDist : DenseVector[(Double, Char)]) : DenseVector[(Double, Char)] ={
 
 
 }
