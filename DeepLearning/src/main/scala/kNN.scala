586d8045516698d789b88772f1d747ce3caf3078
Roxana Tesileanu <roxana.te@web.de>
2017-07-19 12:33:24 +0300

    working at the code for kNN

--- DeepLearning/src/main/scala/kNN.scala
+++ DeepLearning/src/main/scala/kNN.scala
@@ -4,11 +4,11 @@ import breeze.linalg._
 import scala.math._
 import scala.util.Sorting
 
-val group = DenseMatrix((1.0, 1.1), (1.0, 1.0), (0.0, 0.0), (0.0, 0.1))
+val group = DenseVector((1.0, 1.1), (1.0, 1.0), (0.0, 0.0), (0.0, 0.1))
 val lables = DenseVector(Array('A', 'A', 'B', 'B'))
-case class createData (group: DenseMatrix[Double], labels: DenseVector[Char]) 
+case class createData (group: DenseVector[(Double, Double)], labels: DenseVector[Char]) 
 
-val dataSet = createData(group, labels)
+val dataSet = createData(group, lables)
  
 
 
@@ -20,13 +20,17 @@ val d = sqrt(pow(P1(0)- P2(0),2) + pow(P1(1) - P2(1),2))
 d
 }
 
-val distances = dataSet.group map(x => distance(P, x))
+//val distances = dataSet.group map(x => distance(P, x))
 
-val labeledDist = distances.zip(labels)
+//val labeledDist = distances.toScalaVector zip(labels.toScalaVector)
 
-def sortingDist (labeledDist : DenseVector[(Double, Char)]) : DenseVector[(Double, Char)] ={
+//def sortingDist (labeledDist : Vector[(Double, Char)]) : Vector[(Double, Char)] ={
 
 
+//}
+
+
+}
 }
 
 /* References: Machine Learning in Action - Peter Harrington, 2012 
