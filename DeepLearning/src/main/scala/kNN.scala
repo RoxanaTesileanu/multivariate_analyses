b05e3ba3bec8a3bf6e051cc1e6f1471557ba5394
Roxana Tesileanu <roxana.te@web.de>
2017-07-19 13:34:54 +0300

    working at the code for kNN

--- DeepLearning/src/main/scala/kNN.scala
+++ DeepLearning/src/main/scala/kNN.scala
@@ -1,28 +1,28 @@
 object kNN{
 
-import breeze.linalg._
+//import breeze.linalg._
 import scala.math._
 import scala.util.Sorting
 
-val group = DenseVector((1.0, 1.1), (1.0, 1.0), (0.0, 0.0), (0.0, 0.1))
-val lables = DenseVector(Array('A', 'A', 'B', 'B'))
-case class createData (group: DenseVector[(Double, Double)], labels: DenseVector[Char]) 
+val group = Vector((1.0, 1.1), (1.0, 1.0), (0.0, 0.0), (0.0, 0.1))
+val lables = Vector('A', 'A', 'B', 'B')
+case class createData (group: Vector[(Double, Double)], labels: Vector[Char]) 
 
 val dataSet = createData(group, lables)
  
 
 
-class classifykNN (P:DenseVector[(Double, Double)], dataSet: createData, k: Int)  {
+class classifykNN (P:Vector[(Double, Double)], dataSet: createData, k: Int)  {
 
-def distance (P1: DenseVector[(Double,Double)], P2: DenseVector[(Double,Double)]) : Double = {
+def distance (P1: Vector[(Double,Double)], P2: Vector[(Double,Double)]) : Double = {
   
 val d = sqrt(pow(P1(0)._1 - P2(0)._1, 2) + pow(P1(0)._2- P2(0)._2,2))
 d
 }
 
-val distances = dataSet.group map(x => distance(P, DenseVector(x))) 
+val distances = dataSet.group map(x => distance(P, Vector(x))) 
 
-val labeledDist = distances.toScalaVector zip(lables.toScalaVector)
+val labeledDist = distances zip(lables)
 
 //def sortingDist (labeledDist : Vector[(Double, Char)]) : Vector[(Double, Char)] ={
 
@@ -33,5 +33,7 @@ val labeledDist = distances.toScalaVector zip(lables.toScalaVector)
 }
 }
 
-/* References: Machine Learning in Action - Peter Harrington, 2012 
+/* References: 
+Machine Learning in Action - Peter Harrington, 2012 
+Learning Scala - Jason Swartz, 2015
 */
