f33edf32359aa033c77d701474eb3f730e7725d4
Roxana Tesileanu <roxana.te@web.de>
2017-07-20 13:25:20 +0300

    parsing a txt file

---
+++ DeepLearning/src/main/scala/com/incds/scalaML/ReadFile.scala
@@ -0,0 +1,23 @@
+package com.incds.scalaML
+
+object ReadFile {
+
+def readFileClassif (filename: String) : Vector((Double*)), Vector (Char) = {
+
+
+
+
+}
+
+
+
+
+
+
+}
+
+/* References:
+
+Jason Swartz - "Learning Scala", 2015
+
+*/
