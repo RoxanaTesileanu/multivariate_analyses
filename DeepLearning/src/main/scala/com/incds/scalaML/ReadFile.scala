926f53823c575470a794a8421056dd4219a3734e
Roxana Tesileanu <roxana.te@web.de>
2017-07-24 14:02:53 +0300

    generalizing the readFileClassif() to any no. of features

--- DeepLearning/src/main/scala/com/incds/scalaML/ReadFile.scala
+++ DeepLearning/src/main/scala/com/incds/scalaML/ReadFile.scala
@@ -8,16 +8,15 @@ val src = scala.io.Source.fromFile(filename)
 val data = src.getLines.map(_.split(delim)).toArray.drop(1)
 src.close()
 //val len = data(0).length
-val data2 = data.map(x => List(x(0), x(1), x(2), x(3)))
+val data2 = data.map( for (i <- _) yield(i))
 
-
-val data3 = data2.map{ case(List(param1, param2, param3, param4)) => List(param1.toDouble, param2.toDouble, param3.toDouble, param4.toDouble) }
+val data3= data2.map( for (i <- _) yield (i.toDouble))
 
 val dataMatrix= data3.map { case(List(param1, param2, param3, param4)) => List(param1, param2, param3)}
 
 val dataLabels = data3.map { case(List(param1, param2, param3, param4)) => List(param4)}
 
-return (dataMatrix, dataLabels)
+(dataMatrix, dataLabels)
 }
 
 }
