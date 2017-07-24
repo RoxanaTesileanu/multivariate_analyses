b8987a23243b8f5af81b19981c709b44823f4d03
Roxana Tesileanu <roxana.te@web.de>
2017-07-24 09:31:40 +0300

    parsing a txt file for classification

--- DeepLearning/src/main/scala/com/incds/scalaML/ReadFile.scala
+++ DeepLearning/src/main/scala/com/incds/scalaML/ReadFile.scala
@@ -1,14 +1,22 @@
 package com.incds.scalaML
-
+ 
 object ReadFile {
 
-def readFileClassif (filename: String) : Vector((Double*)), Vector (Char) = {
+/*def readFileClassif (filename: String, delim: String) : Vector((Double*)), Vector (Char) = {
 
+val src = scala.io.Source.fromFile(filename)
+val data = src.getLines.map(_.split(delim)).toArray.drop(1)
+src.close()
+val len = data(0).length
+val data2 = data.map(x => List(x(0), x(1), x(2), x(3)))
+val data3 = data2.map{ case(List(param1, param2, param3, param4)) => List(param1.toDouble, param2.toDouble, param3.toDouble, param4.toDouble) }
 
+val dataMatrix= data3.map { case(List(param1, param2, param3, param4)) => List(param1, param2, param3)}
 
+val dataLabels = 
 
 }
-
+*/
 
 
 
@@ -18,6 +26,7 @@ def readFileClassif (filename: String) : Vector((Double*)), Vector (Char) = {
 
 /* References:
 
+Peter Harrington - "Machine Learning in Action", 2012
 Jason Swartz - "Learning Scala", 2015
 
 */
