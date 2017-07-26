97d1ef9e803557ffd4038006ab2ab94aa6011164
Roxana Tesileanu <roxana.te@web.de>
2017-07-26 11:52:22 +0300

    basic vector operations

--- DeepLearning/src/main/scala/com/incds/scalaML/BasicVectorOP.scala
+++ DeepLearning/src/main/scala/com/incds/scalaML/BasicVectorOP.scala
@@ -3,20 +3,36 @@ package com.incds.scalaML
 The BasicVectorOP object offers basic operations for two arrays: dot product, elementwise addition, subtraction, and multiplication. I will extend it to matrices multiplication.    
  
 */
+
 object BasicVectorOP {
 
-def dot (a : Array[Double], b: Array[Double]) : Double = {
 
+def dot (a : Array[Double], b: Array[Double]) : Double = {
 val pairs = a zip b
 val pairProd = for (p <- pairs) yield {p._1 * p._2}
 val dot = pairProd.sum
 dot
 }
 
-def 
+def arrayOP(a: Array[Double], b: Array[Double], f: (Double, Double) => Double) = {
+val pairs = a zip b
+val OP = for (p <- pairs) yield {f(p._1, p._2)}
+OP
+}
 
+def arrayAdd (a: Array[Double], b:Array[Double]) : Array[Double] = {
+val elementwiseAddition = arrayOP(a,b, _ + _)
+elementwiseAddition
+}
 
+def arraySubt (a: Array[Double], b: Array[Double]) : Array[Double] = {
+val elementwiseSubtraction = arrayOP(a,b, _ - _)
+elementwiseSubtraction
+}
 
-
+def arrayMultipl (a: Array[Double], b:Array[Double]) : Array[Double] = {
+val elementwiseMultipl = arrayOP(a,b, _ * _)
+elementwiseMultipl
+}
 
 }
