530562d0daeb168cfadd1df431bf30029001cda9
Roxana Tesileanu <roxana.te@web.de>
2017-07-28 10:11:21 +0300

    adding references

--- DeepLearning/src/main/scala/com/mai/scalaML/BasicVectorOP.scala
+++ DeepLearning/src/main/scala/com/mai/scalaML/BasicVectorOP.scala
@@ -50,6 +50,7 @@ c
 }
 /* References:
 
+Dawkins, P. Paul’s notes on linear algebra. (Lamar University, 2005)
 Swartz Jason 2015 - "Learning Scala", Manning Publications Co., Shelter Island 
 Trask Andrew 2017 - "Grokking deep learning", Manning Publications Co., Shelter Island 
 Odersky Martin, Spoon Lex, Venners Bill 2010 - "Programming in Scala", Second Edition, Artima, Walnut Creek
