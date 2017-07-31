d63fd883662fa77293ed0d76d9af6b3cffea7b25
Roxana Tesileanu <roxana.te@web.de>
2017-07-31 15:16:29 +0300

    working at the autoNorm() function

--- DeepLearning/src/main/scala/com/mai/scalaML/AutoNorm.scala
+++ DeepLearning/src/main/scala/com/mai/scalaML/AutoNorm.scala
@@ -5,7 +5,11 @@ object AutoNorm {
 
 /*def autoNorm (dataMatrix : Vector[Array[Double]]) : Vector[Array[Double]] = {
 
-
+val len = dataMatrix(0).length
+val variables = for (i <- 0 to (len -1)) yield { for (a <- dataMatrix) yield a(i) }
+val normalizedVariables = for (v <- variables) yield { v.map{ a => (a - v.min)/(v.max - v.min)}}
+val minMaxRange = for (v <- variables) yield { (v.min, v.max, v.max - v.min) }
+val normalizedDataMatrix = 
 
 
 }
