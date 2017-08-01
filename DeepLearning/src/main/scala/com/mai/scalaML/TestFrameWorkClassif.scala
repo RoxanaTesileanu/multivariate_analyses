675e1dfb525ed9edf8e6a2ac59b3c9f681ab6568
Roxana Tesileanu <roxana.te@web.de>
2017-08-01 14:28:05 +0300

    working at the testing framework for the kNN classifier

--- DeepLearning/src/main/scala/com/mai/scalaML/TestFrameWorkClassif.scala
+++ DeepLearning/src/main/scala/com/mai/scalaML/TestFrameWorkClassif.scala
@@ -1,4 +1,4 @@
-/* In order to test the kNN classifier you can use the testFrameWorkClassif() function. The error rate is given by the total number of errors (misclassified observations) divied by the total number of tested observations.   
+/* In order to test the kNN classifier you can use the testFrameWorkClassif() function. The error rate is given by the total number of errors (misclassified observations) divied by the total number of tested observations. For testing the Try monadic collection, with its subtypes Success and Failure, will be used.  
 
 */
 
@@ -7,6 +7,7 @@ package com.mai.scalaML
 object TestFrameWorkClassif {
 
 import scala.util.Random._
+import scala.util.{Try, Success, Failure}
 // the shuffle function to shuffle the data first, and then take the first 30% of the obs. for the test set and get the error rate
 
 def testFrameWorkClassif ( ) : ... = {
@@ -26,4 +27,5 @@ def testFrameWorkClassif ( ) : ... = {
 Harrington Peter 2012 - "Machine learning in action", Manning Publications Co., Shelter Island
 Odersky Martin, Spoon Lex, Venners Bill 2010 - "Programming in Scala", Second Edition, Artima, Walnut Creek
 Bugnion Pascal 2016 - "Scala for Data Science", Packt Publishing, Birmingham
+
 */
