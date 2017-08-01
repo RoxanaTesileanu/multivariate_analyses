a2f1c70c567ec675622f01f34f614fff76764bc3
Roxana Tesileanu <roxana.te@web.de>
2017-08-01 13:57:18 +0300

    working at the testing framework for the kNN classifier

---
+++ DeepLearning/src/main/scala/com/mai/scalaML/TestFrameWorkClassif.scala
@@ -0,0 +1,28 @@
+/* In order to test the kNN classifier you can use the testFrameWorkClassif() function. The error rate is given by the total number of errors (misclassified observations) divied by the total number of tested observations.   
+
+*/
+
+package com.mai.scalaML
+
+object TestFrameWorkClassif {
+
+import scala.util.Random._
+
+def testFrameWorkClassif ( ) : ... = {
+
+....
+
+}
+
+
+
+
+
+}
+
+/* References:
+
+Harrington Peter 2012 - "Machine learning in action", Manning Publications Co., Shelter Island
+Odersky Martin, Spoon Lex, Venners Bill 2010 - "Programming in Scala", Second Edition, Artima, Walnut Creek
+
+*/
--- DeepLearning/src/main/scala/com/mai/scalaML/testFrameWorkClassif.scala
+++
@@ -1,1 +0,0 @@
-package com.mai.scalaML
