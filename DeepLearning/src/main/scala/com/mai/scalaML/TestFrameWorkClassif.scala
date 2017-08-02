bc9da32ecfa3381732b1389e1a4d06685d77818a
Roxana Tesileanu <roxana.te@web.de>
2017-08-02 12:12:25 +0300

    working at the testing framework

--- DeepLearning/src/main/scala/com/mai/scalaML/TestFrameWorkClassif.scala
+++ DeepLearning/src/main/scala/com/mai/scalaML/TestFrameWorkClassif.scala
@@ -1,4 +1,4 @@
-/* In order to test the kNN classifier you can use the testFrameWorkClassif() function. The error rate is given by the total number of errors (misclassified observations) divied by the total number of tested observations. For testing the Try monadic collection, with its subtypes Success and Failure, will be used.  
+/* In order to test the kNN classifier you can use the testFrameWorkClassif() function. The error rate is given by the total number of errors (misclassified observations) divied by the total number of tested observations.   
 
 */
 
@@ -7,8 +7,9 @@ package com.mai.scalaML
 object TestFrameWorkClassif {
 
 import scala.util.Random._
+import com.mai.scalaML.kNN._
 
-/*def testFrameWorkClassif ( dataset: CreateData, partition : Double, k :Int) : Double = {
+/*def testFrameWorkClassif ( dataSet: CreateData, partition : Double, k :Int) : Double = {
 
 val dataMatrix = dataSet.dataMatrix
 val dataLabels = dataSet.dataLabels
@@ -18,7 +19,11 @@ val lenDataSet = shuffledDataPlusLabels.length
 val testAndClassSets = shuffledDataPlusLabels.splitAt(lenDataSet * partition)
 val testDataSet = testAndClassSets._1
 val classifDataSet = testAndClassSets._2
-
+val testDataMatrix = testDataSet.map{ x => x._1 }
+val testDataLabels = testDataSet.map{ x => x._2 }
+val classesTest = dataSet.classes
+val usedTestDataSet = new CreateData(testDataMatrix, testDataLabels, classesTest)
+ 
 
 }
 */
@@ -33,5 +38,5 @@ val classifDataSet = testAndClassSets._2
 Harrington Peter 2012 - "Machine learning in action", Manning Publications Co., Shelter Island
 Odersky Martin, Spoon Lex, Venners Bill 2010 - "Programming in Scala", Second Edition, Artima, Walnut Creek
 Bugnion Pascal 2016 - "Scala for Data Science", Packt Publishing, Birmingham
-Scala Standard Library 2.12.0 - scala.util.Try - http://www.scala-lang.org/api/2.12.0/scala/util/Try.html
+Scala Standard Library 2.12.0 -scala.util.Random - http://www.scala-lang.org/api/2.12.1/scala/util/Random.html
 */
