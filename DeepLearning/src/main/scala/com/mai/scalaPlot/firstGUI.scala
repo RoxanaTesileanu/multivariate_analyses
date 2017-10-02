5241b618f68a5b731d106667c0761e6490c6fe85
Roxana Tesileanu <roxana.te@web.de>
2017-10-02 09:38:57 +0300

    extending the statitistics package with plotting methods

---
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/firstGUI.scala
@@ -0,0 +1,21 @@
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+
+object firstGUI extends JFXApp {
+
+	stage = new PrimaryStage{
+		title = "First GUI"
+	}
+}
+
+// in sbt run it with "run FirstGUI.scala"
+
+/* 
+References:
+
+Mark Lewis: Introduction to Programming and Problem Solving using Scala
+https://www.youtube.com/watch?v=LxlNQ6C8WJ4&index=115&list=PLLMXbkbDbVt9MIJ9DV4ps-_trOzWtphYO
+
+Jarek Sacha: Getting started with ScalaFX: Compile and Run
+https://codingonthestaircase.wordpress.com/2013/05/
+*/
---
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/scalaFXfromREPL.scala
@@ -0,0 +1,17 @@
+// this code creates a GUI in REPL
+
+import javax.swing.JFrame
+import javafx.embed.swing.JFXPanel
+import javax.swing.SwingUtilities
+
+val frame = new JFrame ("my frame")
+val panel = new JFXPanel()
+frame.add(panel)
+frame.setSize(600, 600)
+frame.setVisible(true)
+
+/*
+References:
+Irina Fedortsova: Integrating JavaFX into Swing Applications ( http://docs.oracle.com/javafx/2/swing/swing-fx-interoperability.htm )
+
+*/
