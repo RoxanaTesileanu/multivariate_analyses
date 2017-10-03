d15a4aa07176443d905080102fa272d4a8871964
Roxana Tesileanu <roxana.te@web.de>
2017-10-03 08:34:07 +0300

    trying to get over the threading exception

--- DeepLearning/src/main/scala/com/mai/scalaPlot/scalaFXfromREPL.scala
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/scalaFXfromREPL.scala
@@ -7,7 +7,9 @@ package com.mai.scalaPlot
 import javax.swing.JFrame
 import javafx.embed.swing.JFXPanel
 import javax.swing.SwingUtilities
-
+import javax.swing.WindowConstants
+//import scalafx.application.Platform
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object REPL {
 
@@ -15,6 +17,9 @@ val frame = new JFrame ("my frame")
 val panel = new JFXPanel()
 frame.add(panel)
 frame.setSize(600, 600)
+
+frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 frame.setVisible(true)
 // use frame.dispose() if you want to close the GUI
 
@@ -22,9 +27,12 @@ frame.setVisible(true)
 
 /*
 References:
+
+Mark Lewis: SwiftVis2, FXRenderer.scala ( https://github.com/MarkCLewis/SwiftVis2/blob/master/src/main/scala/swiftvis2/plotting/renderer/FXRenderer.scala )
 Irina Fedortsova: Integrating JavaFX into Swing Applications ( http://docs.oracle.com/javafx/2/swing/swing-fx-interoperability.htm )
 
 https://bugs.openjdk.java.net/browse/JDK-8089371
+
 */
 
 
