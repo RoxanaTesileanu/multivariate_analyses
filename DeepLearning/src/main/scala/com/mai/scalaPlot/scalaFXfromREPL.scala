9c2e6d2a01909e79218880b6cdb90e40c03fea16
Roxana Tesileanu <roxana.te@web.de>
2017-10-03 12:18:39 +0300

    trying to get over the threading exception

--- DeepLearning/src/main/scala/com/mai/scalaPlot/scalaFXfromREPL.scala
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/scalaFXfromREPL.scala
@@ -10,30 +10,61 @@ import javafx.embed.swing.JFXPanel
 import javax.swing.SwingUtilities
 //import javax.swing.WindowConstants
 import scalafx.application.Platform
-//import scala.concurrent.Future
-//import scala.concurrent.ExecutionContext.Implicits.global //use Future
-//import scalafx.event.ActionEvent
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global //use Future
+import scalafx.event.ActionEvent
 import scalafx.scene.Scene
 import scalafx.stage.Stage
 import scalafx.scene.canvas.Canvas
 
 object REPL {
 
-Platform.runLater{
-val frame = new JFrame (title)
+
+val frame = new JFrame ("My GUI")
 val panel = new JFXPanel()
 frame.add(panel)
-frame.setSize(size)
+frame.setSize(400, 400)
 
 //frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
 frame.setVisible(true)
 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-}
+
 
 }
 
 // use frame.dispose() if you want to close the GUI
 
+
+
+/*
+scala> javafx.application.Platform.runLater{
+     | val frame = new JFrame("..")
+     | val panel = new JFXPanel()
+     | frame.add(panel)
+     | frame.setVisible(true)
+     | frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)}
+<console>:22: error: type mismatch;
+ found   : Unit
+ required: Runnable
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)}
+
+_____________________________________________________________
+scala> object ShellFX extends JFXApp{}
+defined object ShellFX
+
+scala> ShellFX.
+delayedInit   hostServices   main   stage   stopApp
+
+scala> ShellFX.main
+   def main(args: Array[String]): Unit
+
+scala> Future{ ShellFX}
+res0: scala.concurrent.Future[ShellFX.type] = Future(<not completed>)
+
+scala> val mygui = Future{ShellFX}
+mygui: scala.concurrent.Future[ShellFX.type] = Future(Success(ShellFX$@7b7cf806))
+
+*/
 
 /*
 References:
