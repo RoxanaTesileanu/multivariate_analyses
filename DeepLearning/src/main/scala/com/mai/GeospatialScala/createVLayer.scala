4ca411d80e413ff78eb522195b98319df82256a2
Roxana Tesileanu <roxana.te@web.de>
2017-10-29 18:56:33 +0200

    using GDAL in Scala

--- DeepLearning/src/main/scala/com/mai/GeospatialScala/createVLayer.scala
+++ DeepLearning/src/main/scala/com/mai/GeospatialScala/createVLayer.scala
@@ -8,6 +8,9 @@ object CreateVLayer {
 org.gdal.ogr.ogr.RegisterAll()
 val driver = org.gdal.ogr.ogrJNI.GetDriverByName("ESRI Shapefile")
 val ds = driver.CreateDataSource("rdgps")
+val googleMapSR = new org.gdal.osr.SpatialReference()
+googleMapSR.ImportFromEPSG(
+val lyr = ds.CreateLayer("RoadsAndGPSTracks", googleMapSR, wkbMultiLineString)
 
 
 
