eb28b18055a493c9109e3ae94be8c52e898b71a1
Roxana Tesileanu <roxana.te@web.de>
2017-11-09 13:36:41 +0200

    working at the geostatistical theory perspective

--- DeepLearning/usingLapack2.scala
+++ DeepLearning/usingLapack2.scala
@@ -6,12 +6,12 @@ object UsingLapack2{
 val noArows = 3
 val nrhs = 1
 val matrixA : Array[Array[Double]] = Array(Array(3, 1, 0), Array(-1, 2, 2), Array(5, 0, -1))
-val aoffset = 0
+var aoffset = 0
 val lda = 3
-val ipiv : Array[Int] = Array(0,0,0)
-val ipivoffset = 0
-var b  : Array[Array[Double]] = Array(Array(6, -7, 10))
-val boffset = 0
+var ipiv : Array[Int] = Array(0,0,0)
+var ipivoffset = 0
+var b  : Array[Double] = Array(6, -7, 10)
+var boffset = 0
 val ldb= 3
 var info = new org.netlib.util.intW(0)
 }
