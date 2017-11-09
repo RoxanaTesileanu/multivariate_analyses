64be1cf408c42430073001984a6239e947f86a06
Roxana Tesileanu <roxana.te@web.de>
2017-11-09 09:50:48 +0200

    working at the geostatistical theory perspective

--- DeepLearning/usingLapack.scala
+++ DeepLearning/usingLapack.scala
@@ -2,11 +2,13 @@ import com.github.fommil.netlib.LAPACK.{getInstance => lapack}
 import org.netlib.util.intW._
 
 object UsingLapack{
-val matrixA = Array(Array(3, 1, 0), Array(-1, 2, 2), Array(5, 0, -1))
-val b  = Array(Array(6, -7, 10))
+val matrixA : Array[Array[Double]] = Array(Array(3, 1, 0), Array(-1, 2, 2), Array(5, 0, -1))
+val b  : Array[Array[Double]] = Array(Array(6, -7, 10))
 
 var info = new org.netlib.util.intW(0)
-val ipiv  = 3
+val ipiv : Array[Int] = Array(0,0,0)
 val nrhs = 1
 val noArows = 3
+val lda = 3
+val ldb= 3
 }
