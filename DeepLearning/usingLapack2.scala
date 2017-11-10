9d7893d5abfd2cc7e67bf37e464e839029063f08
Roxana Tesileanu <roxana.te@web.de>
2017-11-10 09:37:36 +0200

    working at the geostatistical theory perspective

--- DeepLearning/usingLapack2.scala
+++ DeepLearning/usingLapack2.scala
@@ -8,13 +8,13 @@ val nrhs = 1
 val matrixA : Array[Array[Double]] = Array(Array(3, 1, 0), Array(-1, 2, 2), Array(5, 0, -1))
 var aoffset = 0
 val lda = 3
-var ipiv : Array[Int] = Array(3,3,3)
+var ipiv : Array[Int] = Array.fill(noArows)(0)
 var ipivoffset = 0
 var b  : Array[Double] = Array(6, -7, 10)
 var boffset = 0
 val ldb= 3
 var info = new org.netlib.util.intW(0)
-lapack.Dgesv.dgesv(noArows, nrhs, matrixA.flatten, aoffset, lda, ipiv, ipivoffset, b, boffset, ldb, info)
+lapack.Dgesv.dgesv(noArows, nrhs, (matrixA.transpose).flatten, aoffset, lda, ipiv, ipivoffset, b, boffset, ldb, info)
 val result = (b, ipiv)
 for (i<- result._1) println (i)
 for (i<- result._2) println (i)
