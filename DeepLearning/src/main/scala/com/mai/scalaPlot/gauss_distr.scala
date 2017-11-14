34699e396376cea2f81ef82005b2f8859b725acf
Roxana Tesileanu <roxana.te@web.de>
2017-11-14 19:54:38 +0200

    working at the geostatistical theory perspective

---
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/gauss_distr.scala
@@ -0,0 +1,11 @@
+import com.panayotis.gnuplot.JavaPlot
+import scala.math._
+
+object GaussDistr{
+
+def Gauss (x: Double, mu:Double, sigma:Double) = 1.0/(sigma*sqrt(2*Pi))*exp( -pow((x-mu),2) / 2/pow((2*sigma),2))
+
+val mu = 50
+val sigma = 100
+val xs = Array.fill(100)({i +=1; i})
+}
