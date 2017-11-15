480c55691f41e0384ef6c8f67e95c06aaa4e5bde
Roxana Tesileanu <roxana.te@web.de>
2017-11-15 11:35:33 +0200

    working at the geostatistical theory perspective

--- DeepLearning/src/main/scala/com/mai/scalaPlot/histo.scala
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/histo.scala
@@ -6,4 +6,14 @@ import com.mai.scalaML._
 import TreeProb._
 
 
+object HistTreeProb {
+
+val classes = Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0)
+val data = classes.zip(probCounts)
+val data2 = data.map( i => Array(i._1, i._2) )
+val ds = new DataSetPlot(data2)
+val p = new JavaPlot
+p.addPlot(ds)
+p.plot
+}
 
