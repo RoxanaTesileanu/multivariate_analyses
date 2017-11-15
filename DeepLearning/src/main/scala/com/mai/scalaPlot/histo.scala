1a0a1cc4a3bb4a94d0ad4b0500e12aa68d07a708
Roxana Tesileanu <roxana.te@web.de>
2017-11-15 11:31:50 +0200

    working at the geostatistical theory perspective

--- DeepLearning/src/main/scala/com/mai/scalaPlot/histo.scala
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/histo.scala
@@ -1,5 +1,6 @@
 import com.panayotis.gnuplot.JavaPlot
 import com.panayotis.gnuplot.style.Style \\ HISTOGRAM
+import com.panayotis.gnuplot.plot.DataSetPlot
 import scala.math._
 import com.mai.scalaML._
 import TreeProb._
