1ac11faea04e5219771f7784b2b5d000bd375906
Roxana Tesileanu <roxana.te@web.de>
2017-11-16 19:22:13 +0200

    working at the geostatistical theory perspective

--- DeepLearning/src/main/scala/com/mai/scalaPlot/unif_distr.scala
+++ DeepLearning/src/main/scala/com/mai/scalaPlot/unif_distr.scala
@@ -1,6 +1,9 @@
 import com.panayotis.gnuplot.JavaPlot
 import scala.math._
 import com.panayotis.gnuplot.plot.DataSetPlot
+import com.panayotis.gnuplot.style.Style
+import com.panayotis.gnuplot.style.PlotStyle
+
 
 object UnifDistr {
 
@@ -12,11 +15,14 @@ var i = 0
 val xs = Array.fill(10)({i +=1; i})
 
 val data = xs.map( i => Unif(i, a, b) )
-
+val data2 = xs.zip(data)
+val data3 = data2.map( i => Array(i._1, i._2) )
 val p = new JavaPlot
-val ds = new DataSetPlot(data)
+val ds = new DataSetPlot(data3)
 val pstyle = ds.getPlotStyle
-pstyle.setStyle(Style.LINES)
+pstyle.setStyle(Style.HISTOGRAMS)
+pstyle.setStyle(Style.HISTEPS)
+p.getAxis("y").setBoundaries(0,1.0)
 p.addPlot(ds)
 p.setTitle("Continuous Uniform Probability Distribution")
 p.setKey(JavaPlot.Key.OFF)
