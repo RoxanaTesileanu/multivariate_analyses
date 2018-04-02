4026eec3d1870a86cf9f83b671ae5bc3aadc55e3
Barbu Tesilean <roxana.te@web.de>
2018-04-02 19:11:48 +0300

    recap intro in Deep Learning

--- DeepLearning/src/main/scala/MultipleInpOut2.scala
+++ DeepLearning/src/main/scala/MultipleInpOut2.scala
@@ -1,3 +1,15 @@
 // I will use for matrix operations the functions from BasicVectorOP.scala
 import com.mai.scalaML.BasicVectorOP._
 
+object MultipleInpOut {
+
+val myWeights = Array(Array(0.1, 0.1, -0.3), Array(0.1, 0.2, 0.0), Array(0.0, 1.3, 0.1))
+val myInputs =  Array(Array(8.5, 0.65, 1.2))
+
+def neural_network (myWeights: Array[Array[Double]], myInputs: Array[Array[Double]]): Array[Array[Double]] = {
+ assert(myWeights(0).length == myInputs(0).length, "Matrices cannot be multiplied!") 
+ val myInTranspose = myInputs.transpose
+ val matMultiplication= matrixMultipl(myWeights, myInputs)
+ matMultiplication
+}
+
