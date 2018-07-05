05e656a18d8e727425ab5c6c2661728a6ec024ce
Barbu Tesilean <roxana.te@web.de>
2018-07-05 11:26:43 +0300

    checking reading different types of sources

--- recap3.scala
+++ recap3.scala
@@ -8,5 +8,11 @@ source.close
 val source2 = Source.FromURL("https://www.....", "iso....")
 read the lines and transform them to an Array
 
-val source3 = getClass.getResource("employees.sql")
+val source3 = getClass.getResource("/employees.sql")
 
+scala> val source3 = getClass.getResource("/employees.sql")
+source3: java.net.URL = file:/home/barbu/multivariate_analyses/DeepLearning/target/scala-2.11/classes/employees.sql
+
+
+// References: Alex Fruzenshtein, http://fruzenshtein.com/scala-working-with-resources-folders-files/
+// and book of Mark Lewis.
