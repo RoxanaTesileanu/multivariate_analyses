07d6ac6520dbeeab344d12c62fa262b94380cb04
Barbu Tesilean <roxana.te@web.de>
2018-07-09 12:48:43 +0300

    creating a SQL DB

--- SqlDB.sql
+++ SqlDB.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE STAFFMEMBERS	#creates a database with name staffmembers
-CREATE TABLE personaldata 	#creates a table within the DB, with the columns id, firstname, lastname, etc.	
+CREATE DATABASE STAFFMEMBERSCOPY	--creates a database with name staffmemberscopy
+CREATE TABLE personaldata 	--creates a table within the DB, with the columns id, firstname, lastname, etc.	
 	(
 	id	SMALLINT NOT NULL,
 	firstname	CHAR(25),
@@ -9,7 +9,7 @@ CREATE TABLE personaldata 	#creates a table within the DB, with the columns id, 
 	startingdate	DATE,
 	salary		DECIMAL(8,2)
 	)
-INSERT INTO personaldata	#insert some rows into personaldata
+INSERT INTO personaldata	--insert some rows into personaldata
 	VALUES (1, "Mike", "Mueller", "m", 3, "01-june-10", 4500.00)
 
 INSERT INTO personaldata
@@ -33,32 +33,32 @@ INSERT INTO personaldata
 INSERT INTO personaldata
 	VALUES (8, "Claude", "Strom", "m",3, "30-june-10", 4000.00)
 
-CREATE TABLE departments	#create a second table within the DB, with two columns: departmentno and departmentname 
+CREATE TABLE departments	--create a second table within the DB, with two columns: departmentno and departmentname 
 	(departmentno	SMALLINT NOT NULL,
 	departmentname	CHAR(15)
-	)
-INSERT INTO departments		# create some rows for the table departments
+	)  
+INSERT INTO departments		-- create some rows for the table departments
 	VALUES (1, "administration")
 INSERT INTO departments
 	VALUES (2, "transport")
 INSERT INTO departments
 	VALUES (3, "pr")
 
-# Some queries:
+-- Some queries:
 
-SELECT * FROM personaldata	#selects all rows of a table, * means all
+SELECT * FROM personaldata	--selects all rows of a table, * means all
 
-SELECT firstname, startingdate FROM personaldata	#selects the two columns (firstname and startingdate) from a table
+SELECT firstname, startingdate FROM personaldata	--selects the two columns (firstname and startingdate) from a table
 
-SELECT * FROM personaldata WHERE departmentno=1		#selects all entries from personaldata where the deptno is 1
+SELECT * FROM personaldata WHERE departmentno=1		--selects all entries from personaldata where the deptno is 1
 
-# instead of = one can use >, <, <> (not equal), <=, >=
+-- instead of = one can use >, <, <> (not equal), <=, >=
 
-SELECT lastname, salary FROM personaldata WHERE salary>4000.00 #selects the lastname for the salary >4000.00
+SELECT lastname, salary FROM personaldata WHERE salary>4000.00       --selects the lastname for the salary >4000.00
 
 SELECT lastname, departmentno, salary FROM personaldata WHERE departmentno=3 AND salary>3000.00
-#selects the lastname from the table with the deptno. 1 and a salary > 4000.00 (two conditions: dept 1 and salar>4000.00)
+--selects the lastname from the table with the deptno. 1 and a salary > 4000.00 (two conditions: dept 1 and salar>4000.00)
 
 
 
-# from Kuhlmann and Muellmerstadt "Datenbanksprache SQL" 1994
+-- from Kuhlmann and Muellmerstadt "Datenbanksprache SQL" 1994
